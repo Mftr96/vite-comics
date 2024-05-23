d8805ffcfe511e733df1fbbe34a048a024070c69
Mftr96 <matteo.federici96@gmail.com>
2024-05-23 15:52:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<script>
+//dall'export importo altri file vue 
+export default {
+  //componenti principali di vue:data,methods e mounted
+  data() {
+    return {
+      nomeApplicazione: "VitEsempio 0.1"
+    }
+  },
+  methods: {
+    //vanno qui, al solito
+  },
+  mounted() {
+    console.log("App montata");
+  }
+}
+</script>
+
+
+<template>
+  <p>ciao, sono vue</p>
+ 
+</template>
+
+<style scoped>
+
+</style>
