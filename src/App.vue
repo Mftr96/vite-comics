2e8a859920e1bb10206e77b49cf81758db7a9747
Mftr96 <matteo.federici96@gmail.com>
2024-05-23 16:38:21 +0200

    lavorazione Appheader

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 
 <script>
-//dall'export importo altri file vue 
+//dall'export importo altri file vue
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+
 export default {
   //componenti principali di vue:data,methods e mounted
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
   data() {
     return {
       nomeApplicazione: "VitEsempio 0.1"
@@ -19,8 +29,9 @@ export default {
 
 
 <template>
-  <p>ciao, sono vue</p>
- 
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <style scoped>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,56 @@
 <script>
+export default {
+  data() {
+    return {
+        headerList:[
+            "CHARACTERS",
+            "COMICS",
+            "MOVIES",
+            "TV",
+            "GAMES",
+            "COLLECTIBLES",
+            "VIDEOS",
+            "FANS",
+            "NEWS",
+            "SHOP",
+        ]
+    }
+  },
+  methods: {
+    //funzioni Vue 
+  },
+  mounted() {
+    console.log("App montata Header");
+  }
+}
+
+
+
 </script>
+
 <template>
+    <header>
+        <img src="../../vue-dc-comics-1/vue-dc-comics-1/img/dc-logo.png" alt="dc-logo.png">
+        <div class="header-right">
+            <a v-for="link in headerList" href="#">{{link}}</a>
+        </div>
+
+    </header>
 
 </template>
 
 <style scoped>
+header{
+    display: flex;
+    justify-content: space-around;
+    padding: 2rem;
+}
 
+.header-right{
+    padding: 0.3rem;
+    display: flex;
+    align-items: center;
+    gap: 0.4rem;
+
+}
 </style>
